148ef2f69be9030d061a99384f346cc713254101
onlytoseef <hellotoseef@gmail.com>
2025-05-03 16:28:04 +0500

    loops

---
+++ datatypes/examples.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+int rana (){
+	
+	string typeOfTea = " Lemon \t";
+	float priceofTea = 99.788 ;
+	char ratingOfTea = 'A';
+	
+	
+	
+	cout << "Your tea" <<typeOfTea << "is" << priceofTea << "per kg \t" << "or its rating is \t" << ratingOfTea << endl;
+
+
+	
+	
+
+	
+	
+	
+	
+
+	 
+	
+}
+
+int main (){
+	
+	
+	
+	cout << "this is rana funciton" << endl;
+	rana();
+	return 0;
+	
+	
+}
+
+
+
+
+
+
---
+++ datatypes/loops.cpp
@@ -0,0 +1,29 @@
+// loops 
+
+// While loops
+#include <iostream>
+#include <string>
+
+
+
+using namespace std;
+
+
+
+int main(){
+	
+	int teaCups;
+	
+	
+	cout << "Ap kitny chai k cup lena pasand krein gy ?";
+	cin >> teaCups;
+	
+	while (teaCups>0){
+		cout << "Serving cup of tea \n" << teaCups << "remaining" << endl;
+		teaCups--;
+      }    
+	cout << "Sari Chai ap pi chuky hein "<< endl;
+	
+	
+	
+}
